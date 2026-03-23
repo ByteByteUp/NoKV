6aaf71ddc1d5b056856d39dca0a3ca9a5be601d9
ByteByteUp <ByteByteUp@users.noreply.github.com>
2026-03-23 14:16:19 +0000

    deploy: 6cae04c7f98a31da0212ed1998c2b86303251db0

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-4a2244d7.js',
+                path_to_root + 'searchindex-bd28ccbb.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
